25cee3b400c76737b8fd6486ff58880706f32cba
neildegrassepython <acassidy@openmailbox.org>
2016-03-09 17:12:08 +0000

    Delete euler-one.py

--- Problem-One/euler-one.py
+++
@@ -1,9 +0,0 @@
-# Find the sum of every multiple of 3 or 5 under 1000
-
-multiples = []
-
-for i in range (0, 1000):
-    if i % 3 == 0 or i % 5 == 0:
-        multiples.append(i)
-
-print(sum(multiples))
